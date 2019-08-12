1bea0d799f1a0e0770203b13b4d4e0d7504435a8
Jackson Tubbs <decknot@Jacksons-MacBook-Pro.local>
2019-08-12 11:01:13 -0600

    Creates folder structure

---
+++ DeskOfCards/DrawViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DrawViewController.swift
+//  DeskOfCards
+//
+//  Created by Jackson Tubbs on 8/12/19.
+//  Copyright © 2019 Jax Tubbs. All rights reserved.
+//
+
+import UIKit
+
+class DrawViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var suitLabel: UILabel!
+    @IBOutlet weak var cardImageView: UIImageView!
+    
+    // MARK: - Lifecycle Functions
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func drawCardButtonTapped(_ sender: Any) {
+        
+    }
+} // End Of Class
