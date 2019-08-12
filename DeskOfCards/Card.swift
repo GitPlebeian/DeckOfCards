c32e67c6c6c1433ed8294a359862ff4bb4ab68e8
Jackson Tubbs <decknot@Jacksons-MacBook-Pro.local>
2019-08-12 12:52:11 -0600

    Finishes project

---
+++ DeskOfCards/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  DeskOfCards
+//
+//  Created by Jackson Tubbs on 8/12/19.
+//  Copyright © 2019 Jax Tubbs. All rights reserved.
+//
+
+import Foundation
---
+++ DeskOfCards/CardController.swift
@@ -0,0 +1,9 @@
+//
+//  CardController.swift
+//  DeskOfCards
+//
+//  Created by Jackson Tubbs on 8/12/19.
+//  Copyright © 2019 Jax Tubbs. All rights reserved.
+//
+
+import Foundation
